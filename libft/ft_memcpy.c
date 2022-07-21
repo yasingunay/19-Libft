3a101bbe3ba9a0d09d20fd395124f081754fa2d3
yasingunay <av.yasingunay@gmail.com>
2022-07-21 12:07:58 +0200

    update

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 10:40:21 by ygunay            #+#    #+#             */
-/*   Updated: 2022/07/21 12:05:32 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/07/21 12:07:50 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,18 @@ psrc=(char *)src;
 	return (dst);
 }
 
-int		main(void)
-{
-	char	dst[] = "123456789";
-	char	src[] = "hello";
+// int		main(void)
+// {
+// 	char	dst[] = "123456789";
+// 	char	src[] = "hello";
 	
-	char	*ret;
-	char	*ret2;
+// 	char	*ret;
+// 	char	*ret2;
 	
-	ret = ft_memcpy(dst, src, 5);
-	ret2 = memcpy(dst, src, 5);
-	printf("new dest(ft): %s\n", ret);
-	printf("new dest(or): %s\n", ret2);
+// 	ret = ft_memcpy(dst, src, 5);
+// 	ret2 = memcpy(dst, src, 5);
+// 	printf("new dest(ft): %s\n", ret);
+// 	printf("new dest(or): %s\n", ret2);
 	
-	return (0);
-}
+// 	return (0);
+// }
