8cf997eff4a66652eec5868ed20e10d3ed1480e9
yasingunay <chalouper@icloud.com>
2022-07-22 09:59:46 +0200

    update

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 10:40:21 by ygunay            #+#    #+#             */
-/*   Updated: 2022/07/18 10:43:20 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/07/22 09:59:11 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
      The memcpy() function copies n bytes from memory area src to memory area
      dst.  If dst and src overlap, behavior is undefined.  Applications in
      which dst and src might overlap should use memmove(3) instead.
-
 RETURN VALUES
      The memcpy() function returns the original value of dst.
 */
@@ -23,23 +22,30 @@ RETURN VALUES
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int	i;
+	size_t	i;
+	char	*pdst;
+	char	*psrc;
 
+	pdst = (char *)dst;
+	psrc = (char *)src;
 	i = 0;
-	while (i > n)
+	while (i < n)
 	{
-		((char *) dst)[i] = ((char *) src)[i];
+		pdst[i] = psrc[i];
 		i++;
 	}
 	return (dst);
 }
 
-// int main () {
-//    const char src[50] = "yasingunay";
-//    char dest[50];
-//    strcpy(dest,"Heloooo!!");
-//    printf("Before memcpy dest = %s\n", dest);
-//    ft_memcpy(dest, src, strlen(src)+1);
-//    printf("After memcpy dest = %s\n", dest);
-//    return(0);
+// int		main(void)
+// {
+// 	char	dst[] = "123456789";
+// 	char	src[] = "hello";
+// 	char	*ret;
+// 	char	*ret2;
+// 	ret = ft_memcpy(dst, src, 5);
+// 	ret2 = memcpy(dst, src, 5);
+// 	printf("new dest(ft): %s\n", ret);
+// 	printf("new dest(or): %s\n", ret2);
+// 	return (0);
 // }
