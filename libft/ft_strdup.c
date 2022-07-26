7f86bc1392befc2ba1bdc4cd1215fda1784b81d2
yasingunay <chalouper@icloud.com>
2022-07-26 11:49:41 +0200

    add

---
+++ libft/ft_strdup.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 11:15:51 by ygunay            #+#    #+#             */
+/*   Updated: 2022/07/26 11:47:11 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+	   The strdup() function returns a pointer to a new string which is
+       a duplicate of the string s.  Memory for the new string is
+       obtained with malloc(3), and can be freed with free(3).
+
+RETURN VALUE         
+       On success, the strdup() function returns a pointer to the
+       duplicated string.  It returns NULL if insufficient memory was
+       available, with errno set to indicate the error.
+*/
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dst;
+	size_t	i;
+	size_t	size;
+
+	size = strlen(s) + 1;
+	dst = (char *) malloc (size * sizeof (char));
+	if (!dst)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s[i])
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+// int main(void)
+// {
+// char *p1= "yasin";
+// char *p2;
+// p2=ft_strdup(p1);
+// printf("duplicated string is : %s\n", p2);
+// return (0);
+// }
