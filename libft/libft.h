8411adde36ec31ab561c1c107624f06dc05a445b
yasingunay <chalouper@icloud.com>
2022-08-01 12:23:46 +0200

    add

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:45:39 by ygunay            #+#    #+#             */
-/*   Updated: 2022/07/26 18:54:33 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/08/01 08:23:42 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ int		ft_isprint(int c);
 int		ft_isascii(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
