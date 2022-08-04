a8e173e223f0a3ee69c853c988495c4db419103b
yasingunay <chalouper@icloud.com>
2022-08-04 08:48:40 +0200

    update

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 17:44:06 by ygunay            #+#    #+#             */
-/*   Updated: 2022/07/18 17:58:33 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/08/04 08:48:22 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,34 +23,23 @@ RETURN VALUES
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*str;
-	unsigned char	x;
-	size_t			i;
+	unsigned char	*ret;
 
-	i = 0;
-	str = (unsigned char *)s;
-	x = (unsigned char)c;
-	while (i < n)
+	ret = (unsigned char *)s;
+	while (n--)
 	{
-		if (str[i] == x)
-			return ((void *)s + i);
-		i++;
+		if (*ret == (unsigned char)c)
+			return (ret);
+		ret++;
 	}
 	return (NULL);
 }
 
-// int main () 
+// int main(void)
 // {
-// 	const char str[] = "yasin.gunay.123";
-// 	const char ch = '.';
-// 	char *ret1;
-// 	char *ret2;
-
-// 	ret1 = ft_memchr(str, ch, 3);
-// 	ret2 = memchr(str, ch,3);
-
-// 	printf("String after |%c| is - |%s|\n", ch, ret1);
-//  	printf("String after |%c| is - |%s|\n", ch, ret2);
-
-// 	return(0);
+// char str[]="yasin";
+// char str2[]="yasin";
+// printf("or %s\n",memchr(str,'s',4));
+// printf("ft %s\n",ft_memchr(str2,'s',4));
+// return (0);
 // }
