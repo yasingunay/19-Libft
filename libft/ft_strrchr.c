623694e25dafc7115441467aa1fbfbccba9e4977
yasingunay <chalouper@icloud.com>
2022-08-05 08:22:15 +0200

    update

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,9 +6,25 @@
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 10:21:29 by ygunay            #+#    #+#             */
-/*   Updated: 2022/08/05 08:21:02 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/08/05 08:21:55 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+*	LIBRARY
+*	#include <string.h>
+*	DESCRIPTION
+*	The strchr() function locates the last occurrence of c (converted to a 
+*	char) in the string pointed to by s. The terminating null character is 
+*	considered to be part of the string; therefore if c is `\0', the functions 
+*	locate the terminating `\0'.
+*	PARAMETERS
+*	#1. The string in which to check the occurence of c.
+*	#2. The character to check the occurence of.
+*	RETURN VALUES
+*	The function strrchr() returns a pointer to the located 
+*	character, or NULL if the character does not appear in the string.
+*/
 
 #include "libft.h"
 
