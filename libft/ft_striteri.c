061f535a005ead0a517c8121c902b3a46c55ec19
yasingunay <chalouper@icloud.com>
2022-08-08 08:17:17 +0200

    update

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 10:45:40 by ygunay            #+#    #+#             */
-/*   Updated: 2022/07/28 11:35:12 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/08/08 08:11:23 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	}
 }
 
-void	ft_to_lowercase(unsigned int i, char *ch)
-{
-	printf("s[%d] is %c\n", i, ch[0]);
-	if (ch[0] >= 65 && ch[0] <= 90)
-		ch[0] = ch[0] + 32;
-}
+// void	ft_make_big(unsigned int i, char *c)
+// {
+// 	printf("s[%d] is %c\n", i, c[0]);	
+// 	if (c[0] >= 97 && c[0] <= 122)
+// 			c[0] = c[0] - 32;
+// }
 
 // int main(void)
 // {
